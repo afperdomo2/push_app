57d08088e4cb02f1734dfb8ada8ed8c7d6cd9798
alvarado0925 <alvarado0925@gmail.com>
2025-05-27 23:24:44 -0500

    Actualizar el manejo del estado de notificaciones y obtener el token FCM al cambiar el estado

--- lib/features/home/blocs/notifications/notifications_bloc.dart
+++ lib/features/home/blocs/notifications/notifications_bloc.dart
@@ -9,15 +9,33 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   NotificationsBloc() : super(const NotificationsInitial()) {
-    on<NotificationStatusChanged>(_changeNotificationStatus);
+    on<NotificationStatusChanged>(_updateNotificationState);
+
+    _initializeNotificationStatus();
+  }
+
+  void _initializeNotificationStatus() async {
+    final settings = await messaging.getNotificationSettings();
+    add(NotificationStatusChanged(settings.authorizationStatus));
+  }
+
+  void _getFCMToken() async {
+    final settings = await messaging.getNotificationSettings();
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      final token = await messaging.getToken();
+      print('🔑FCM Token: $token');
+    } else {
+      print('❌Notifications are not authorized.');
+    }
   }
 
   /// Cambia el estado de las notificaciones.
-  void _changeNotificationStatus(
+  void _updateNotificationState(
     NotificationStatusChanged event,
     Emitter<NotificationsState> emit,
   ) {
     emit(state.copyWith(status: event.status));
+    _getFCMToken();
   }
 
   /// Solicita permiso para enviar notificaciones al usuario.
