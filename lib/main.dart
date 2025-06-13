6ee9a5332eee80cf3e4ca0b3b52b39cd141d5c81
alvarado0925 <alvarado0925@gmail.com>
2025-06-12 22:52:50 -0500

    Agregar manejador para mensajes de Firebase en segundo plano

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:push_app/config/router/app_router.dart';
@@ -6,12 +7,25 @@ import 'package:push_app/config/theme/app_theme.dart';
 import 'package:push_app/features/home/blocs/notifications/notifications_bloc.dart';
 import 'package:push_app/firebase_options.dart';
 
+/// Este manejador se ejecuta cuando la aplicación está en segundo plano o cerrada
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // Si vas a utilizar otros servicios de Firebase en segundo plano, como Firestore,
+  // asegúrate de llamar a `initializeApp` antes de usar otros servicios de Firebase.
+  await Firebase.initializeApp();
+
+  // ignore: avoid_print
+  print('Manejando un mensaje en segundo plano: ${message.messageId}');
+}
+
 void main() async {
   // Inicializar Firebase y WidgetsFlutterBinding
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Configurar Firebase Messaging para manejar mensajes en segundo plano
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(
     MultiBlocProvider(
